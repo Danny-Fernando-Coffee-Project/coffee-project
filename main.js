2b27db45b0ca85d37de9f01dd8bd69ed1f7c97cb
Fernando Lopez <flopez00032@gmail.com>
2022-09-16 16:16:03 -0500

    search bar functionality

--- main.js
+++ main.js
@@ -55,3 +55,12 @@ let roastSelection = document.querySelector('#roast-selection');
 tbody.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
+
+
+//search bar functionality
+let searchBar = document.querySelector("#searchBar");
+let searchButton = document.querySelector("#firstSubmit");
+
+searchButton.addEventListener('click',function (){
+    alert(searchBar.value)
+})
